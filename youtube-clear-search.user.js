24663ab69dcf5d324e0f02e225cdf69d7ce8c4de
tiger31 <tiger31839@gmail.com>
2023-07-09 21:57:29 +0400

    youtube-clear-search.user.js

---
+++ youtube-clear-search.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         youtube-clear-search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Clear youtube serach
+// @author       github.com/tiger31
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+const targetSelectors = [
+    'ytd-reel-shelf-renderer', //shorts
+    'ytd-shelf-renderer', // suggestions
+    'ytd-horizontal-card-list-renderer' //another suggestions?
+];
+
+(function() {
+    'use strict';
+    const hideFn = () => {
+        const hideTargets = targetSelectors.reduce((acc, selector) => {
+            acc.push(...document.getElementsByTagName(selector));
+            return acc;
+        }, []);
+        for (let i of hideTargets) {
+            i.style.display = "none";
+        }
+    }
+    const handleMutation = (mutationsList, observer) => {
+        if (mutationsList.some(m => m.type === 'childList')) {
+            hideFn();
+        }
+    }
+
+    const observer = new MutationObserver(handleMutation);
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
